4164ffdee986f245f56c1b7211cb8fab84994d96
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-11-01 17:16:12 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const newTask = document.getElementById('new-task-description')
+  let i = 0
+  document.querySelector('form').addEventListener('submit', (e) => {
+    
+    e.preventDefault()
+    let li = document.createElement('li')
+    tasks.append(li)
+    li.append(newTask.value)
+    
+    let bttn = document.createElement('button')
+    bttn.className=`del`
+    let txt = document.createTextNode(' X ')
+    bttn.appendChild(txt)
+    li.appendChild(bttn)
+
+    const deleteButtons = document.querySelectorAll('.del'); 
+
+    deleteButtons.forEach( button => {
+      button.addEventListener('click', removeMe); 
+      });
+    })
+  });
+
+const tasks = document.getElementById('tasks');
+
+function removeMe() {
+  this.closest('li').remove();
+};
